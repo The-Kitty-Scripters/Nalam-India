0b6b07ba8166b348e7a0c195854ecae594d614ed
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-11-12 10:19:44 +0900

    init

---
+++ nalam/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from nalam.users.models import User
+from nalam.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()

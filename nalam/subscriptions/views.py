b7f63b3a78d05bb4187894bcfe5b752bf2ca6052
Michael <mherman315@gmail.com>
2023-11-14 10:08:14 -0800

    flak8 errors fixed

--- nalam/subscriptions/views.py
+++ nalam/subscriptions/views.py
@@ -1,11 +1,13 @@
 import stripe
 from django.conf import settings
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
 
+from nalam.email_sendgrid.views import send_thank_you_email
 from nalam.subscriptions.models import PaymentHistory
+from nalam.users.admin import User
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -21,7 +23,7 @@ def create_checkout_session(request):
             checkout_session = stripe.checkout.Session.create(
                 line_items=[
                     {
-                        "price": "price_1OBUBpKD3FKdsp6aNadoMppm",
+                        "price": "price_1OBij0L3io6xXWmkXVuFRxJ7",
                         "quantity": 1,
                     },
                 ],
@@ -46,9 +48,9 @@ def stripe_webhook(request):
     try:
         event = stripe.Webhook.construct_event(payload, sig_header, stripe_webhook_endpoint)
     except ValueError as e:
-        return HttpResponse(status=400)
+        return HttpResponse(e, status=400)
     except stripe.error.SignatureVerificationError as e:
-        return HttpResponse(status=400)
+        return HttpResponse(e, status=400)
 
     # Passed signature verification
 
@@ -58,22 +60,26 @@ def stripe_webhook(request):
         # ---------------- mikey --------------------- #
         # this is generally where you create the order and mark it as awaiting payment
         # Something.create_order(session["id"])
-
         session_id = session["id"]
         customer_email = session["customer_details"]["email"]
+
+        user = User.objects.create(name=session["customer_details"]["name"], email=customer_email)
+
         # note: ignore this I just hard coded it b/c I didn't feel like converting it to decimals
-        # amount = session["amount_total"]
+        amount = session["amount_total"]
 
         payment_history = PaymentHistory.objects.create(
             subscription_id="",
             session_id=session_id,
-            product_id="price_1OBUBpKD3FKdsp6aNadoMppm",
-            amount_total=9999.99,
+            product_id="price_1OBij0L3io6xXWmkXVuFRxJ7",
+            amount_total=399.99,
         )
 
         if session.payment_status == "paid":
             # this is where you fulfill the order
             # TODO: create and send email, see line 64
-            pass
+            send_thank_you_email(user, amount)
+            payment_history.paid = True
+            payment_history.save
 
     return HttpResponse(status=200)
--- nalam/users/tasks.py
+++ nalam/users/tasks.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 
 from config import celery_app
+from nalam.email_sendgrid.views import send_thank_you_email
 
 User = get_user_model()
 
@@ -9,3 +10,8 @@ User = get_user_model()
 def get_users_count():
     """A pointless Celery task to demonstrate usage."""
     return User.objects.count()
+
+
+@celery_app.task()
+def send_email(d):
+    send_thank_you_email(d)
